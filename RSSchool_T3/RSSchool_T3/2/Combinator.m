fb27f75b3ebd350bce4d1740c1ddf32b5eb669d8
agapkinadiana <agapkinadiana@mail.ru>
2020-04-04 00:06:29 +0300

    feat: add solution for 2 task

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -1,7 +1,28 @@
 #import "Combinator.h"
 
 @implementation Combinator
+
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    
+    NSInteger m = [array[0] intValue];
+    NSInteger n = [array[1] intValue];
+    
+    if ([array count] != 2 || !array || m < 0 || n <= 0) return nil;
+    
+    for (int i = 0; i < n; i++) {
+        NSInteger c = [self factorial:n] / ([self factorial:i] * [self factorial:n - i]);
+        
+        if (c == m) return @(i);
+    }
+    
+    return nil;
 }
+
+- (NSInteger)factorial:(NSInteger)number {
+    
+    if (number == 0) return 1;
+    
+    return number * [self factorial:number - 1];
+}
+
 @end
